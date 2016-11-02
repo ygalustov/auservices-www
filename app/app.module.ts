72512b0ae169f33e424d0e50d67bf4dd869fde2d
Yury Galustov <ygalustov@gmail.com>
2016-11-03 00:03:35 +1100

    Header and Section #1 updated

--- app/app.module.ts
+++ app/app.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header.component';
+import { WhatIsItComponent } from "./whatisit.component";
 
 @NgModule({
     imports: [BrowserModule],
-    declarations: [AppComponent, HeaderComponent],
+    declarations: [AppComponent, HeaderComponent, WhatIsItComponent],
     bootstrap: [AppComponent]
 })
 export class AppModule {
---
+++ app/whatisit.component.ts
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) Yury Galustov <ygalustov@gmail.com All rights reserved. Licensed under the MIT license.
+ * See LICENSE in the project root for license information.
+ */
+
+import { Component } from "@angular/core";
+
+@Component({
+    selector: "whatisit",
+    templateUrl: "app/whatisit.component.html",
+    styleUrls: ["css/whatisit.css"]
+})
+export class WhatIsItComponent {
+}
